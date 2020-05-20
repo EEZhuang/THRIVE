ab5bd3c40ee2218b02ed2a5d49b24dc97c4ba532
Edwardxie72 <47434661+Edwardxie72@users.noreply.github.com>
2020-05-19 19:01:45 -0700

    Finished Goal Screen UI (#6)
    * goal form, need to do collabs
    
    * collabs added and returned
    
    * collabs finished with multi select
    
    * Fixed error with back button on collabs

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:thrive/screens/createGoal/createGoal.dart';
 import 'package:thrive/services/auth.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -44,7 +45,10 @@ class _HomeState extends State<Home> {
     } else if (_selectedIndex == 1) {
       widget.toggleState(2);
     } else if (_selectedIndex == 2) {
-
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => CreateGoal()),
+      );
     } else if (_selectedIndex == 3) {
       widget.toggleState(4);
     }
