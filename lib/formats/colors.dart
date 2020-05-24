db608fc22c72ed39035cbc1d882b9056bcb0eed6
EEZhuang <43657787+EEZhuang@users.noreply.github.com>
2020-05-23 22:04:08 -0700

    Emily (#13)
    * Included the bottom navigation bar with 3 icons (home, add goal, and profile). Buttons do not redirect user to different screens yet.
    
    * Included the bottom navigation bar with 4 icons (home, search, goal, and profile). Buttons do not redirect user to different screens yet.
    
    * Added navigation bar to the bottom of the current create goal screen. Buttons do not redirect to other screens
    
    * Progress on navbar
    
    * Navbar navigates between a stateless widget and a stateful widget
    
    * Added profile, social wall, and add goal screens to nav bar
    
    * Cleaned up wrapper.dart file
    
    * UI tests
    
    * colors format file
    
    Co-authored-by: Emily Zhuang <e.jinghua.zhuang@ucsd.edu>

---
+++ lib/formats/colors.dart
@@ -0,0 +1,15 @@
+library colors;
+import 'package:flutter/material.dart' as Material;
+
+const String SUCCESS_MESSAGE=" You will be contacted by us very soon.";
+
+const Material.Color BLACK = Material.Color(0xFF080F0F);
+const Material.Color TRANSPARENT_BLACK = Material.Color(0xFF080F0F);
+const Material.Color DARK_GRAY = Material.Color(0xFF27353F);
+const Material.Color DARK_GREEN = Material.Color(0xFF69A297);
+const Material.Color LIGHT_GREEN = Material.Color(0xFFC2EAC6);
+const Material.Color WHITE = Material.Color(0xFFFAF9F9);
+const Material.Color LIGHT_ORANGE = Material.Color(0xFFFFD6BA);
+const Material.Color DARK_ORANGE = Material.Color(0xFFFE752B);
+
+
