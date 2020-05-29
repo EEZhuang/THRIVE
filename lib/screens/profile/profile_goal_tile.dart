45379d3c56dcc9096025c443f657c1b9c1fdef2d
Edwardxie72 <47434661+Edwardxie72@users.noreply.github.com>
2020-05-29 02:37:45 -0700

    new goal update progress, numbers clamp from [-10, 10], updates in db

--- lib/screens/profile/profile_goal_tile.dart
+++ lib/screens/profile/profile_goal_tile.dart
@@ -25,61 +25,55 @@ class _GoalTileState extends State<GoalTile> {
     DateTime now = new DateTime.now();
     DateTime todayDate = new DateTime(now.year, now.month, now.day);
     // TODO: implement build
-    return Padding(
-        padding: EdgeInsets.only(top: 1.0),
-        child: Card(
-          margin: EdgeInsets.fromLTRB(20, 6, 20, 0),
-          child: ListTile(
-            leading: CircularPercentIndicator(
-                radius: 50,
-                animation: true,
-                animationDuration: 1200,
-                lineWidth: 5.0,
-                // indicates progress on goal
-                percent: (double.parse(widget.goal.goalProgress) /
-                    double.parse(widget.goal.goalUnits)),
-                center: new Text(
-                  widget.goal.goalUnits,
-                  style: new TextStyle(
-                    fontFamily: 'proxima',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20.0,
-                    color: ThriveColors.DARK_GREEN,
-                  ),
-                ),
-                backgroundColor: ThriveColors.DARK_GREEN,
-                circularStrokeCap: CircularStrokeCap.round,
-                progressColor: ThriveColors.DARK_ORANGE),
-            title: Text(
-              widget.goal.goal,
-              style: new TextStyle(
-                  fontFamily: 'proxima',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20.0,
-                  color: realDate.isBefore(todayDate) ? ThriveColors.DARK_ORANGE : Colors.black,
-              ),
-            ),
-            subtitle: Text(
-                widget.goal.goalDate,
-                style: new TextStyle(
-                  color: realDate.isBefore(todayDate) ? ThriveColors.DARK_ORANGE : ThriveColors.DARK_GRAY,
-                ),
-            ),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                EditGoalProgress(
-                    goal: widget.goal,
-                    id: widget.id,
-                    updateTile: widget.updateTile),
-                EditGoal(
-                    goal: widget.goal,
-                    id: widget.id,
-                    updateTile: widget.updateTile)
-              ],
+    return ListTile(
+      leading: CircularPercentIndicator(
+          radius: 50,
+          animation: true,
+          animationDuration: 1200,
+          lineWidth: 5.0,
+          // indicates progress on goal
+          percent: (double.parse(widget.goal.goalProgress) /
+              double.parse(widget.goal.goalUnits)),
+          center: new Text(
+            widget.goal.goalUnits,
+            style: new TextStyle(
+              fontFamily: 'proxima',
+              fontWeight: FontWeight.bold,
+              fontSize: 20.0,
+              color: ThriveColors.DARK_GREEN,
             ),
           ),
-          color: Color(0xFFF0FFED),
-        ));
+          backgroundColor: ThriveColors.DARK_GREEN,
+          circularStrokeCap: CircularStrokeCap.round,
+          progressColor: ThriveColors.DARK_ORANGE),
+      title: Text(
+        widget.goal.goal,
+        style: new TextStyle(
+            fontFamily: 'proxima',
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0,
+            color: realDate.isBefore(todayDate) ? ThriveColors.DARK_ORANGE : Colors.black,
+        ),
+      ),
+      subtitle: Text(
+          widget.goal.goalDate,
+          style: new TextStyle(
+            color: realDate.isBefore(todayDate) ? ThriveColors.DARK_ORANGE : ThriveColors.DARK_GRAY,
+          ),
+      ),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          //EditGoalProgress(
+          //    goal: widget.goal,
+          //    id: widget.id,
+          //    updateTile: widget.updateTile),
+          EditGoal(
+              goal: widget.goal,
+              id: widget.id,
+              updateTile: widget.updateTile)
+        ],
+      ),
+    );
   }
 }
